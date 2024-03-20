a418d018bb67c9f5d09578250598f20d1b965ac4
Harsh/dvedi <89665648+hrdv10@users.noreply.github.com>
2024-03-21 01:12:54 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>upsc preps, stratergies </title>
+        <title> game development blogs  </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
